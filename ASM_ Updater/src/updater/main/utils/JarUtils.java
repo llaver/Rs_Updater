6aae6d3a353e15c6fc8a3e3eabb51e0f5b8a2878
Llaver <Llaver@live.com>
2013-04-11 11:48:51 -0500

    Update JarUtils.java

--- ASM_ Updater/src/updater/main/utils/JarUtils.java
+++ ASM_ Updater/src/updater/main/utils/JarUtils.java
@@ -9,28 +9,34 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
 
 public class JarUtils {
-	
+
 	public HashMap<String, ClassNode> parseJar(JarFile jar) {
 		HashMap<String, ClassNode> classes = new HashMap<String, ClassNode>();
 		try {
 			Enumeration<?> enumeration = jar.entries();
-			//Iterate over every file in the jar
-			while(enumeration.hasMoreElements()) {
+			// Iterate over every file in the jar
+			while (enumeration.hasMoreElements()) {
 				JarEntry entry = (JarEntry) enumeration.nextElement();
-				//If this file isn't a class, it will be skipped.
-				if(entry.getName().endsWith(".class")) {
-					//Will Parse the class file
-					ClassReader classReader = new ClassReader(jar.getInputStream(entry));
+				// If this file isn't a class, it will be skipped.
+				if (entry.getName().endsWith(".class")
+						&& !entry.getClass().getPackage().getName().contains(
+								"jag")
+						&& !entry.getClass().getPackage().getName().contains(
+								"jac")) {
+					// Will Parse the class file
+					ClassReader classReader = new ClassReader(jar
+							.getInputStream(entry));
 					ClassNode classNode = new ClassNode();
-					classReader.accept(classNode, ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
-					//Add the file to the HashSet
+					classReader.accept(classNode, ClassReader.SKIP_DEBUG
+							| ClassReader.SKIP_FRAMES);
+					// Add the file to the HashSet
 					classes.put(classNode.name, classNode);
-				}				
-			}			
+				}
+			}
 			jar.close();
 			return classes;
 		} catch (Exception e) {
 			return null;
-		}		
+		}
 	}
 }
